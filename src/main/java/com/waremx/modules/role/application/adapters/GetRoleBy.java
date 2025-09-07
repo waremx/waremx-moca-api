f1744145255a734ae2d0e62999577eb13de595b9
ymoyamac <yael.moya.mac@outlook.com>
2025-09-07 09:42:22 -0600

    feat(role): Add FilterInputHandler to validate input format

--- src/main/java/com/waremx/modules/role/application/adapters/GetRoleBy.java
+++ src/main/java/com/waremx/modules/role/application/adapters/GetRoleBy.java
@@ -7,7 +7,10 @@ import com.waremx.common.mox.core.Prop;
 import com.waremx.common.mox.uni.Context;
 import com.waremx.modules.role.application.repositories.RoleRepository;
 import com.waremx.modules.role.domain.contexts.RoleContext;
-import com.waremx.modules.role.domain.handlers.GetRoleByName;
+import com.waremx.modules.role.domain.enums.RoleEvents;
+import com.waremx.modules.role.domain.enums.RoleKeys;
+import com.waremx.modules.role.domain.handlers.FilterInputHandler;
+import com.waremx.modules.role.domain.handlers.GetRoleByNameHandler;
 import com.waremx.modules.role.domain.objects.Role;
 import com.waremx.modules.role.infrastructure.rest.dtos.RoleDto;
 import io.vavr.control.Either;
@@ -24,15 +27,17 @@ public class GetRoleBy implements GetByService<String, RoleDto> {
 
     @Override
     public Either<MocaErrCodes, RoleDto> getByService(String roleName) {
+        String event = RoleEvents.EVENT_GET_ROLE_BY.getEvent();
         Context<Role, MocaErrCodes> context = new Context<>();
         RoleContext roleContext = new RoleContext();
-        context.subscribe("get_role_by", roleContext);
-        context.set(Prop.bind("action", "get_by"));
-        context.set(Prop.bind("role_name", roleName));
+        context.subscribe(event, roleContext);
+        context.set(Prop.bind(RoleKeys.ACTION.getKey(), event));
+        context.set(Prop.bind(RoleKeys.IN_ROLE_NAME.getKey(), roleName));
 
         Context last = Handler.link(
-                        new GetRoleByName(roleRepository)
-                )
+               new FilterInputHandler(event),
+               new GetRoleByNameHandler(roleRepository)
+        )
                 .execute(context)
                 .build();
 
---
+++ src/main/java/com/waremx/modules/role/domain/enums/RoleKeys.java
@@ -0,0 +1,16 @@
+package com.waremx.modules.role.domain.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum RoleKeys {
+    ACTION("action"),
+    IN_ROLE_NAME("in_role_name"),
+    IN_CREATE_ROLE_DTO("in_create_role_dto")
+    ;
+
+    private final String key;
+    RoleKeys(String key) {
+        this.key = key;
+    }
+}
